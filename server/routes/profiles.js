df464ef4cb01b88d1c20d9700e0ed6e52a32f659
Jessica Thompson <jthompson0670@gmail.com>
2018-02-01 10:37:23 -0800

    broken

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,51 +1,70 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const ProfileController = require('../controllers').Profiles; //requiring the methods
+const ProfileController = require('../controllers').Company; //requiring the methods
 const config = require('../../config/development.json')
+const app = express() //
+const path = require('path')//
+const pg = require('pg')
 
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:3000/';
+const middleware = require('../middleware');
 const Twit = require('twit');
 const helper = require('../controllers/profiles.js')
-var T = new Twit({
-  consumer_key:         'hPwQLGT14IDfrhKJ6FtjVYni7',
-  consumer_secret:      'RE1jam20D7J4whwh94TT1vPddPfyhq8Gye5DQZAoXqFI5fdO3t',
-  access_token:         '957040105226555392-VCJq4UtXbn5xqG8jsWUHSm4zFKMzuc0',
-  access_token_secret:  'DrGtYziXg38BaNmvlj2w9JkaXQffciScncga0ANSSJwcF',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
 
-var stream = T.stream('statuses/sample')
+//
+app.set('view enginer', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
-let params = { q: 'bob', count: 3, until: '2018-01-19'  }
-// let params = { q: 'yes since:2011-07-11', count: 3, until: '2018-01-19'  }
-//q is used for the search, count is how many tweets, time...etc
-//use time ago 5 min for 3 months
 router.route('/')
-T.get('search/tweets', params, function(err, data, response) {
-  let tweets = data.statuses;
-  let formattedData = {}
-  // console.log(data.statuses[0].user.name)
-  for(var i = 0; i < tweets.length; i++){
-    formattedData = {
-      id: tweets[i].id,
-      user: tweets[i].user.screen_name,
-      message: tweets[i].text,
-      time: tweets[i].created_at
-    }
+.get(ProfileController.getAll) //running methods from controllers/profiles.js
+// .post(ProfileController.create)
+  // T.get('search/tweets', params, function(err, data, res) {
+  //   let tweets = data.statuses;
+  //   // let company = {};
+  //   // create dummy data
+  //     company.id = 1
+  //     company.name = "Facebook"
+  //     console.log(company)
+  //     // res.json(company)///why doesnt res.json work?
+  //
+  // });
+    // response.render("company", {company: companyData});
+//next get tweets about the company to render with ejs
 
-    // console.log(formattedData)
-  }
 
-console.log(data.statuses[0].user.name)
-  //data.statuses.user ==== all data about user
-  // console.log(data)
-  // res.save(777)
-})
-//set up schema here to format data to be saved to the db//
 
-  // .get(ProfileController.getAll) //running methods from controllers/profiles.js
-  // .post(ProfileController.create)
-  ;
+  // let formattedData = {}
+  // for(var i = 0; i < tweets.length; i++){
+  //   formattedData = {
+  //     id: tweets[i].id_str,
+  //     user: tweets[i].user.screen_name,
+  //     message: tweets[i].text,
+  //     time: tweets[i].created_at
+  //   }
+  //   console.log(formattedData)
+  // }
+
+
+
+// let getData = T.get('statuses/user_timeline', params, function(err, data, res){
+//   data.forEach(user => {
+//     // let name = user.user.name.split(" ")
+//     let name = user.user.name.replace(/[^\w\s]/gi, '').split(' ')
+//     name = name.filter((n) => { return n.length >= 1 });
+//     let formattedData = {
+//      user_id: user.user.id_str,
+//      first: name[0],
+//      last: name[name.length - 1],
+//      display: user.user.screen_name,
+//      message: user.text
+//     }
+//     console.log(formattedData)
+//   })
+// })
+
+
+
 
 router.route('/:id')
   .get(ProfileController.getOne)
@@ -54,3 +73,4 @@ router.route('/:id')
   ;
 
 module.exports = router;
+// module.exports = message;
