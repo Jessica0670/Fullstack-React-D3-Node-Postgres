ce17655dd1d22a231196ab6ba81a465f38453423
Jessica Thompson <jthompson0670@gmail.com>
2018-02-12 12:22:16 -0800

    created function to handle sentiment data, not finished

--- client/src/components/bar.js
+++ client/src/components/bar.js
@@ -10,50 +10,44 @@ class Chart extends React.Component {
       data: []
     };
   }
+
   componentWillReceiveProps(){ //works!
-    this.setState({ data: this.props.data })
-    console.log(this.props.data, 'DATA IN COMPONENT CHART line 15')
+    // this.setState({ data: this.props.data }) //does not work
+    console.log(this.props.data, 'DATA IN COMPONENT barjs from props line 15')
+    let mappedData = this.props.data.map(x => {
+      return x * 10
+    })
+    console.log(mappedData, 'testing mapped data from prop bar js line 20')
+    // run function to load data to y value in barData
+    graphLoader(mappedData)
   }
-  render() {
 
-
-    // const BarChart = rd3.BarChart;
-const LineChart = rd3.LineChart;
-// const PieChart = rd3.PieChart;
-// const AreaChart = rd3.AreaChart;
-// const Treemap = rd3.Treemap;
-// const ScatterChart = rd3.ScatterChart;
-// const CandleStickChart = rd3.CandleStickChart;
-    var barData = [
-      {
-        "name": "Series A",
-        "values": [
-          { "x": 1, "y":  91},
-          { "x": 2, "y": 100},
-          { "x": 3, "y": 20},
-          { "x": 4, "y": -20},
-          { "x": 5, "y": -110},
-          { "x": 6, "y": 34},
-          { "x": 7, "y": 12},
-          { "x": 8, "y": 12},
-          { "x": 9, "y": 12},
-          { "x": 10, "y": 12}
-
-        ]
+  //create function to load data to y value in barData
+    graphLoader(graphDataArray){
+      if(graphDataArray.length < 1){
+        return 'empty data line 28 barjs'
       }
-      // ,
-      // {
-      //   "name": "Series B",
-      //   "values": [
-      //     { "x": 1, "y":  9},
-      //     { "x": 2, "y": 49},
-      //     { "x": 3, "y": -80},
-      //   ]
-      // }
-    ];
+      barData.name = "Company"
+      barData.values = []
+      console.log(graphDataArray, 'graph data array l 32 barjs')
+      let count = 1
+      graphDataArray.forEach(item => {
+        let a = {}
+        a.x = count++;
+        a.y = item
+        barData.values.push(a)
+      })
+      return barData
+    }
+
+
+  render() {
+    let barData = [];
+    const LineChart = rd3.LineChart;
+
 
     return  (<div>
-      <p>{this.props.data, 'line 56 in bar component'}</p>
+      <p>{this.props.data, 'this.props.data line 50 in bar component'}</p>
       <LineChart
         data={barData}
         width={800}
